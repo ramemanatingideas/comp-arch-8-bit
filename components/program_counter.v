53cc7c8e17ed042fdcd3472d3d9213791980085c
Ram Narayanan <ramnarayanan51@gmail.com>
2024-03-13 21:08:22 -0400

    Added verilog for PC

--- components/program_counter.v
+++ components/program_counter.v
@@ -1,10 +1,25 @@
-module moduleName (
-    input addr[7:0],
-    output reg [7:0]
+module ProgramCounter(
+    input wire clk,       
+    input wire reset,   
+    input wire enable,  
+    output reg [7:0] pc
 );
 
-always @(posedge clk ) begin
-    
+ // Register to hold the program counter value
+reg [7:0] pc_reg;
+
+
+always @(posedge clk  or posedge reset ) begin
+    if (reset) begin
+            // Reset the program counter value
+            pc_reg <= 8'b00000000;
+        end else if (enable) begin
+            // Increment the program counter
+            pc_reg <= pc_reg + 1;
+    end 
 end
+
+assign pc = pc_reg;
+
     
 endmodule
