2745f9dfdecb1d1b762f83b74532dbff1d5c9d9f
Ram Narayanan <ramnarayanan51@gmail.com>
2024-04-02 07:49:59 +0530

    Update control_unit.v
    Modified control_unit

--- components/control_unit.v
+++ components/control_unit.v
@@ -1,48 +1,45 @@
-module control_unit(input [1:0] opcode,
-                    output reg ALUop,
-                    output reg RegWrite,
-                    output reg MemWrite,
-                    output reg MemRead,
-                    output reg ALUSrc);
-    always @* begin
-        begin
-            case (opcode)
-                2'b00: begin
-                    ALUop = 2'b00
-                    RegWrite = 0
-                    MemWrite = 0
-                    MemRead = 0
-                    ALUSrc = 0
-                end
-                2'b01: begin
-                    ALUop = 2'b00
-                    RegWrite = 1
-                    MemWrite = 0
-                    MemRead = 1
-                    ALUSrc = 0
-                end
-                2'b10: begin
-                    ALUop = 2'b00
-                    RegWrite = 0
-                    MemWrite = 1
-                    MemRead = 0
-                    ALUSrc = 0
-                end
-                2'b11: begin
-                    ALUop = 2'b00
-                    RegWrite = 0
-                    MemWrite = 0
-                    MemRead = 0
-                    ALUSrc = 0
-                end
-
-                default: begin
-                    ALUop = 2'b00
-                    RegWrite = 0
-                    MemWrite = 0
-                    MemRead = 0
-                    ALUSrc = 0
-                end
-            endcase
-        end
-    end
+    `timescale 1ns / 1ps
+    
+    module control_unit(input [1:0] opcode,
+                        output reg RegWrite,
+                        output reg MemWrite,
+                        output reg MemRead,
+                        output reg ALUSrc);
+        always @* begin
+            begin
+                case (opcode)
+                    2'b00: begin
+                        RegWrite = 0;
+                        MemWrite = 0;
+                        MemRead = 0;
+                        ALUSrc = 0;
+                    end
+                    2'b01: begin
+                        RegWrite = 1;
+                        MemWrite = 0;
+                        MemRead = 1;
+                        ALUSrc = 1;
+                    end
+                    2'b10: begin
+                        RegWrite = 0;
+                        MemWrite = 1;
+                        MemRead = 0;
+                        ALUSrc = 1;
+                    end
+                    2'b11: begin
+                        RegWrite = 1;
+                        MemWrite = 0;
+                        MemRead = 0;
+                        ALUSrc = 1;
+                    end
+    
+                    default: begin
+                        RegWrite = 0;
+                        MemWrite = 0;
+                        MemRead = 0;
+                        ALUSrc = 0;
+                    end
+                endcase
+            end
+         end
+    endmodule
