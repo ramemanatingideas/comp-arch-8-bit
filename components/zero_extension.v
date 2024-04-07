2ada24feab31876492938f5aab335189ef99edc0
Ram Narayanan <ramnarayanan51@gmail.com>
2024-04-08 03:43:20 +0530

    Update zero_extension.v
    Mofieid zero_extension to the version we want.

--- components/zero_extension.v
+++ components/zero_extension.v
@@ -1,11 +1,32 @@
-module zero_extension (
-    input [7:0] input_data,       // Input data to be zero-extended
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01.04.2024 21:30:50
+// Design Name: 
+// Module Name: zero_extension
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module zero_extension(
+    input [1:0] input_data,       // Input data to be zero-extended
     output reg [7:0] output_data  // Zero-extended output data
 );
-
     // Zero extension operation
     always @* begin
-        output_data = input_data;  // Output data is same as input data for zero extension
+//        output_data = {{6{input_data[1]}}, input_data};
+        output_data = {6'b0, input_data};
     end
-
 endmodule
