d6e560e79a64af2cd7fbe7fdd71ac1ddd923b234
Ram Narayanan <ramnarayanan51@gmail.com>
2024-04-08 03:44:03 +0530

    Update data_memory.v
    Mofieid data memory to include writing only on negedge

--- components/data_memory.v
+++ components/data_memory.v
@@ -1,28 +1,49 @@
 `timescale 1ns / 1ps
-
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01.04.2024 21:55:47
+// Design Name: 
+// Module Name: data_memory
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 module data_memory(
-
-  input clk,                
-  input [7:0] data_in,       
-  input wr,                  
-  input rd,                  
-  input [7:0] add,          
-  output reg [7:0] data_out 
+  input clk,                 // Clock input
+  input [7:0] data_in,       // Data input to be written to memory
+  input wr,                  // Write control signal
+  input rd,                  // Read control signal
+  input [7:0] addr,           // Address input
+  output reg [7:0] data_out  // Data output read from memory
 );
 
- reg [7:0] memory [0:255];  
-  always @(posedge clk) begin
+ reg [7:0] memory [0:255];  // Declaration of 256x8-bit RAM array
+
+  always @(negedge clk) begin
+    $display("write control signal: wr: %b, Address: %b", wr, addr);
+    $display("data_in: %b", data_in);
     if (wr) begin
-      memory[add] <= data_in;
+      memory[addr] <= data_in;  // Write operation: Store data_in to ram at address add
     end
   end
-
-  always @(*) begin
+ 
+  always @(addr or rd) begin
+   $display("read control signal (only reading): rd: %b with addr %b", rd, addr);
     if (rd) begin
-      data_out = memory[add]; 
+      data_out = memory[addr];  // Read operation: Output data from ram at address add
     end
     else begin
-      data_out = 8'bzzzzzzzz;
+      data_out = 8'bzzzzzzzz;      // If rd is low, output high-impedance ("don't care" state)
     end
   end
 
