81a4858e5b090559d646defc880aa0f18a9fa7aa
Ram Narayanan <ramnarayanan51@gmail.com>
2024-04-08 03:42:55 +0530

    Update mux.v
    Modified to invlude parameterized bit setting for the mux.

--- components/mux.v
+++ components/mux.v
@@ -1,7 +1,13 @@
-module mux(input [8:0] a,
-               input [8:0] b,
-               input  sel,
-               output [8:0] out );
+module mux2 #(parameter BIT_LEN=8) 
+         (     input [BIT_LEN-1:0] input1,
+               input [BIT_LEN-1:0] input2,
+               input  select,
+               output [BIT_LEN-1:0] out );
+    assign out = select == 0 ? input1 : input2;
+    
+//    always @* begin
+//      $display("MUX input1: %b, input2: %b, selection: %b, out: %b", input1, input2, select, out);
+//    end
+  
 
-    assign out = sel == 0 ? a : b;
-endmodule
+endmodule 
