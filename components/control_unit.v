af2691c12d4d8d7de85ba2786b506eb134f7785f
Ram Narayanan <ramnarayanan51@gmail.com>
2024-04-08 03:44:18 +0530

    Update control_unit.v

--- components/control_unit.v
+++ components/control_unit.v
@@ -1,43 +1,50 @@
-    `timescale 1ns / 1ps
-    
-    module control_unit(input [1:0] opcode,
+`timescale 1ns / 1ps
+
+ module control_unit(input [1:0] opcode,
                         output reg RegWrite,
                         output reg MemWrite,
                         output reg MemRead,
-                        output reg ALUSrc);
+                        output reg ALUSrc,
+                        output reg RegWriteSrc
+                        );
         always @* begin
             begin
                 case (opcode)
                     2'b00: begin
-                        RegWrite = 0;
+                        RegWrite = 1;
                         MemWrite = 0;
                         MemRead = 0;
                         ALUSrc = 0;
+                        RegWriteSrc = 0;
                     end
                     2'b01: begin
                         RegWrite = 1;
                         MemWrite = 0;
                         MemRead = 1;
                         ALUSrc = 1;
+                        RegWriteSrc = 1;
                     end
                     2'b10: begin
                         RegWrite = 0;
                         MemWrite = 1;
                         MemRead = 0;
                         ALUSrc = 1;
+                        RegWriteSrc = 0;
                     end
                     2'b11: begin
                         RegWrite = 1;
                         MemWrite = 0;
                         MemRead = 0;
                         ALUSrc = 1;
+                        RegWriteSrc = 0;
                     end
     
                     default: begin
-                        RegWrite = 0;
+                        RegWrite = 1;
                         MemWrite = 0;
                         MemRead = 0;
                         ALUSrc = 0;
+                        RegWriteSrc = 0;
                     end
                 endcase
             end
