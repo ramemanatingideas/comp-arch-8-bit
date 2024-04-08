3880a5b87a9b24902d8df149f062855b58edacc5
Ram Narayanan <ramnarayanan51@gmail.com>
2024-04-09 04:48:02 +0530

    Update data_memory.v

--- components/data_memory.v
+++ components/data_memory.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 01.04.2024 21:55:47
-// Design Name: 
-// Module Name: data_memory
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 module data_memory(
   input clk,                 // Clock input
   input [7:0] data_in,       // Data input to be written to memory
@@ -30,17 +11,17 @@ module data_memory(
  reg [7:0] memory [0:255];  // Declaration of 256x8-bit RAM array
 
   always @(negedge clk) begin
-    $display("write control signal: wr: %b, Address: %b", wr, addr);
-    $display("data_in: %b", data_in);
+//    $display("write control signal: wr: %b, Address: %b", wr, addr);
+//    $display("data_in: %b", data_in);
     if (wr) begin
-      memory[addr] <= data_in;  // Write operation: Store data_in to ram at address add
+      memory[addr] <= data_in; 
     end
   end
  
   always @(addr or rd) begin
-   $display("read control signal (only reading): rd: %b with addr %b", rd, addr);
+//   $display("read control signal (only reading): rd: %b with addr %b", rd, addr);
     if (rd) begin
-      data_out = memory[addr];  // Read operation: Output data from ram at address add
+      data_out = memory[addr];  
     end
     else begin
       data_out = 8'bzzzzzzzz;      // If rd is low, output high-impedance ("don't care" state)
