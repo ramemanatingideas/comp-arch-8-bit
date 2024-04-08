be61701253d4242d3c6d8eccf67739917eb94cd5
Ram Narayanan <ramnarayanan51@gmail.com>
2024-04-09 04:49:10 +0530

    Update alu.v to separate out Addi instruction with ALUSrc

--- components/alu.v
+++ components/alu.v
@@ -2,11 +2,13 @@
 module alu (
     input [7:0] rs_data1,     // Input data from the first source register
     input [7:0] rs_data2,     // Input data from the second source register
-    input [1:0] immediate,    // Immediate value for SW and LW instructions
     input [1:0] alu_op,       // ALU operation code
+    input alu_src,            // Determine if its immediate or Rtype
     output reg [7:0] result   // Output result of ALU operation
 );
-
+    initial begin
+     result = 8'b00000000;
+    end 
     // Define parameters for ALU operation codes
     parameter ADD = 2'b00;     // Addition
     parameter SUB = 2'b01;     // Subtraction
@@ -14,16 +16,18 @@ module alu (
     parameter AND = 2'b11;     // Bitwise AND
 
     // Perform ALU operation based on alu_op
-    always @* begin
-        case (alu_op)
-            ADD: result = rs_data1 + rs_data2;    // Perform addition
-            SUB: result = rs_data1 - rs_data2;    // Perform subtraction
-            SLL: result = rs_data1 << rs_data2;  // Perform left logical shift
-            AND: result = rs_data1 & rs_data2;   // Perform bitwise AND
-            2'b10: result = {rs_data1[7:immediate], rs_data1[6:0]}; // SW: Store the 8-bit value from rs_data1 to memory
-            2'b11: result = {8'b0, rs_data1[7:immediate]};          // LW: Load a word from memory to register
-            default: result = 8'b0;                                  // Default value
-        endcase
+    always @(*) begin
+        if(alu_src) begin
+            result = rs_data1 + rs_data2;
+        end else begin
+            case (alu_op)
+                ADD: result = rs_data1 + rs_data2;    // Perform addition
+                SUB: result = rs_data1 - rs_data2;    // Perform subtraction
+                SLL: result = rs_data1 << rs_data2;  // Perform left logical shift
+                AND: result = rs_data1 & rs_data2;   // Perform bitwise AND
+                default: result = 8'b0 ;// Default value is no op
+            endcase
+        end
     end
 
 endmodule
