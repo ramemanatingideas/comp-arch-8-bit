17fe600851ca4ba9809e8c0d499d1470c9ef8d89
Mayur Patil <47498537+Mayur-Patil-2305@users.noreply.github.com>
2024-04-09 22:10:06 -0400

    Update zero_extension.v

--- components/zero_extension.v
+++ components/zero_extension.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 01.04.2024 21:30:50
-// Design Name: 
-// Module Name: zero_extension
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module zero_extension(
     input [1:0] input_data,       // Input data to be zero-extended
